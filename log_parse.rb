6e3584d8b4bf80f2a32d42f0502a9acbfac979c7
Brian Murphy <brian.murphy@spongecell.com>
2013-10-02 14:48:25 -0700

    Group-by

--- log_parse.rb
+++ log_parse.rb
@@ -33,44 +33,75 @@ class LogParser
 
         request['query'] = query
 
-        @format.each do |spec|
+        hash = @format.reduce({}) do |hash, spec|
           source = spec.split('.').reduce(request) do |hash, key|
             hash[key] || ''
           end
+          hash[spec] ||= source
 
-          totals[spec] ||= {}
-          target = totals[spec]
-
-          target[source] ||= 0
-          target[source] += 1
+          hash
         end
+        hash_key = JSON.generate(hash)
+
+        totals[hash_key] ||= {hash: hash, total: 0}
+        totals[hash_key][:total] += 1
+        
+        # @format.each do |spec|
+        #   source = spec.split('.').reduce(request) do |hash, key|
+        #     hash[key] || ''
+        #   end
+
+        #   totals[spec] ||= {}
+        #   target = totals[spec]
+
+        #   target[source] ||= 0
+        #   target[source] += 1
+        # end
       end
     end
 
     table = []
     widths = {}
     
-    totals.keys.each do |metric|
-      totals[metric].each do |key, value|
-        table << {metric => key, total: value}
-        widths[metric] ||= key.size
-        widths[metric] = [widths[metric], key.size].max
+    totals.values.each do |row|
+      row[:hash].each do |spec, value|
+        widths[spec] ||= 0
+        widths[spec] = [widths[spec], spec.size, value.size].max
       end
     end
+
     @format.each do |spec|
       size = [widths[spec], spec.size].max
       printf("%#{size}s ", spec)
     end
-    # printf "\n"
-    # puts table
-    table.each do |row|
-      row.each do |key, value|
-        size = widths[key] || 0
-        printf("%#{size}s ", value)
-      end
-      puts("\n")
+    puts "Total"
 
+    totals.values.each do |row|
+      row[:hash].each do |spec, value|
+        printf("%#{widths[spec]}s ", value)
+      end
+      puts row[:total]
     end
+
+    # totals.keys.each do |metric|
+    #   totals[metric].each do |key, value|
+    #     table << {metric => key, total: value}
+    #     widths[metric] ||= key.size
+    #     widths[metric] = [widths[metric], key.size].max
+    #   end
+    # end
+    # @format.each do |spec|
+    #   size = [widths[spec], spec.size].max
+    #   printf("%#{size}s ", spec)
+    # end
+    # table.each do |row|
+    #   row.each do |key, value|
+    #     size = widths[key] || 0
+    #     printf("%#{size}s ", value)
+    #   end
+    #   puts("\n")
+
+    # end
   end
 end
 
