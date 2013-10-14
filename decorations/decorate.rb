784c0e304e31b6511147902689964250cb1097c7
Brian Murphy <brian.murphy@spongecell.com>
2013-10-13 17:11:16 -0700

    Decorations

---
+++ decorations/decorate.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'date'
+require 'active_support/all'
+require 'uri'
+require 'cgi'
+require 'json'
+require 'csv'
+require 'trollop'
+require 'redis'
+
+def log_line_regex
+  /(?<ip_address>[0-9\.]+) - - \[(?<timestamp>.+?)\] "(?<uri>.+?)" (?<status>[0-9]+) [0-9]+ "(?<referrer>.+?)" "(?<user_agent>.+?)"/
+end
+
+redis = Redis.new(:host => "localhost", :port => 6378)
+
+file = open("status.oct102013.log")
+
+file.each_line do |line|
+  log_line_regex.match(line) do |matches|
+    ip = matches[:ip_address]
+
+    addrInfo = redis.hgetall(ip)
+    
+    puts "#{line} #{addrInfo["country"]} #{addrInfo["metroCode"]} #{addrInfo["speed"]}"
+  end
+end
